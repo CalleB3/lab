6432ae40afdabe8307bcfee2e1710b48bf6c688d
Carl Andersson <carl.andersson@xenit.se>
2021-11-23 13:06:19 +0100

    Adding files to config

---
+++ config/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group = "rg-we-dev-webserver"
+location       = "west europe"
---
+++ modules/webserver/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group" {
+    type = string
+}
+variable "location" {
+    type = string
+}
+variable "virtual_machine_name" {
+    type = string
+}
+
+variable "virtual_machine_size" {
+    type = string
+}
+variable "virtual_network_name" {
+    type = string
+}
+variable "virtual_network_interface" {
+    type = string
+}
